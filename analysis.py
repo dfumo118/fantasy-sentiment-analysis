fee95ef9b0bffd65235ef149031c6d7453af10e9
David Fumagalli <davidfumo18@gmail.com>
2022-08-09 20:05:24 -0400

    Finalized pip installing

--- analysis.py
+++ analysis.py
@@ -8,10 +8,10 @@ import nltk
 import pycountry
 import re
 import string
-from wordcloud import WordCloud, STOPWORDS
 from PIL import Image
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from langdetect import detect
 from nltk.stem import SnowballStemmer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import CountVectorizer
+
