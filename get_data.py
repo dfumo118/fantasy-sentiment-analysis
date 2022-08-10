fa6faa9963493e0f98cb2a93aff31ebb1248a274
David Fumagalli <davidfumo18@gmail.com>
2022-08-10 11:34:13 -0400

    Added start_date / end_date to data getter

--- get_data.py
+++ get_data.py
@@ -17,19 +17,31 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import CountVectorizer
 import json
 import csv
+import datetime
 
 file = open("config.json")
 config = json.load(file)
 file.close()
 
 players = {}
+start_date = ""
+end_date = ""
 with open("player_data.csv") as f:
     r = csv.reader(f, delimiter=',')
     line_count = 0
     for row in r:
-        if line_count != 0:
+        if line_count == 0:
+            last_date = row[len(row)-1]
+            month = int(last_date[0:-2])
+            year = int(last_date[-2:])
+
+            start_date = datetime.datetime(2000+year, month+1, 1).strftime("%Y-%m-%dT00:00:00Z")
+            end_date = (datetime.datetime(2000+year, month+2, 1) - datetime.timedelta(days=1)).strftime("%Y-%m-%dT00:00:00Z")
+
+        else:
             players[row[0]] = []
         line_count += 1
 
 client = tweepy.Client(bearer_token=config['bearerToken'])
 
+# make query using players, start, end
