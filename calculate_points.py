e2a8d50a5cab45a68306391cdc34bf4931ce5c0f
David Fumagalli <davidfumo18@gmail.com>
2022-08-26 14:39:09 -0400

    Changes to correct naming processes in nfl-data

--- calculate_points.py
+++ calculate_points.py
@@ -3,14 +3,12 @@ import nfl_data_py as nfl
 def get_rosters():
     df = nfl.import_rosters([2021], 
         [
-            'first_name',
-            'last_name',
+            'player_name',
             'player_id'
         ]
     )
 
     return df
-    # Mac Jones -> 00-0036972
 
 def get_fantasy_points(player, week):
     rosters = get_rosters()
@@ -24,7 +22,8 @@ def get_fantasy_points(player, week):
     )
 
     df = weekly.merge(rosters)
+    row = df.loc[df['player_name'] == player].loc[df['week'] == week]
+    if row.empty:
+        return 0
 
-    first, last = player.split(' ')
-
-    return float(df.loc[df['last_name'] == last].loc[df['first_name'] == first].loc[df['week'] == week]['fantasy_points_ppr'])
+    return float(row['fantasy_points_ppr'])
