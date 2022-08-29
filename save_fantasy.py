336189fc8f05f9e272b02758ec1fd034387bf355
David Fumagalli <davidfumo18@gmail.com>
2022-08-29 10:50:22 -0400

    Further adding more csv files

---
+++ save_fantasy.py
@@ -0,0 +1,11 @@
+from cmath import nan
+import get_data as gd
+import interact_csv as ic
+
+if __name__ == "__main__":
+    df = ic.csv_to_df("player_fantasy_data.csv")
+    players = ic.get_players(df) 
+    row = [x for x in df.iloc[0] if str(x) != 'nan']
+    week = len(row) + 1
+
+    points_df = gd.pull_fantasy_data(players, week)
